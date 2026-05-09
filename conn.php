e79d10ccd8cb63509ac2ba76ce1e35e0f7b8c812
minhnv010212 <minhnv010212@gmail.com>
2026-05-09 17:02:02 +0700

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,17 +1,31 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-// $username = "tbigshop_decor";
-// $password = "!@#SONbang123";
+
+$servername = "levudecor-db-designlevu.k.aivencloud.com";
+$port = "27885";
+$dbname = "defaultdb";
+
+$username = "avnadmin";
+$password = "AVNS_kjgqFTzs9TwJOqwc2xL";
 
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=design_levu;charset=utf8", $username, $password);
-  $conn->exec("set names utf8");
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  "Connected successfully";
+
+    $conn = new PDO(
+        "mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8",
+        $username,
+        $password
+    );
+
+    $conn->exec("set names utf8");
+
+    $conn->setAttribute(
+        PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION
+    );
+
 } catch(PDOException $e) {
-  echo "Connection failed: " . $e->getMessage();
+
+    die("Connection failed: " . $e->getMessage());
+
 }
+
 ?>
